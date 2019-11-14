463d5cfccd1d6e704c7d2963d8a7ba5b1d473f54
VlphaL3wis <32800870+VlphaL3wis@users.noreply.github.com>
2019-11-14 17:10:17 -0600

    We created an obstacle

---
+++ Ice Cold Beer Unity Project/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public GameObject gameOverText;
+    private AudioSource audioSource;
+
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();   
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D collison)
+    {
+        gameOverText.SetActive(true);
+        audioSource.Play();
+        Debug.Log("Has touched the obstacle");
+    }
+}
